b0bf9d2ebc9ff7115ee65cc7d4ba698049e2a2a4
Matt Hamilton <m@tthamilton.com>
2015-12-20 20:41:54 -0500

    load-time optimizations

--- modules/pacman/init.zsh
+++ modules/pacman/init.zsh
@@ -1,5 +1,12 @@
 #
-# Pacman aliases and functions
+# Pacman aliases
+#
+
+# ${zpacman_frontend} is provided by either .zimrc or (if not set) init.zsh
+# The zpacman_frontend is _only_ used for package installs.
+
+#
+# Setup
 #
 
 # ensure pacman is available
@@ -26,6 +33,96 @@ elif (( ! ${+commands[${zpacman_frontend}]} )); then
   zpacman_frontend='pacman'
 fi
 
+#
+# General
+#
+
+alias pac=${zpacman_frontend}
+
+#
+# Build
+#
+
+# build package in current directory, cleanup, and install
+alias pacb='makepkg -sci'
+
+#
+# Install
+#
+
+#NOTE: Installing/upgrading individual packages is NOT supported. Sync and upgrade ALL on install.
+
+# install, sync, and upgrade packages
+alias paci='sudo ${zpacman_frontend} -Syu'
+
+# install, sync, and upgrade packages (forcibly refresh package lists)
+alias pacu='sudo ${zpacman_frontend} -Syyu'
+
+# install packages by filename
+alias pacU='sudo ${zpacman_frontend} -U'
+
+# install all packages in current directory
+alias pacd='sudo ${zpacman_frontend} -U *.pkg.tar.xz'
+
+
+#
+# Remove
+#
+
+# remove package and unneeded dependencies
+alias pacr='sudo pacman -R'
+
+# remove package, unneeded dependencies, and configuration files
+alias pacrm='sudo pacman -Rns'
+
+
+#
+# Query
+#
+
+# query package information from the remote repository
+alias pacq='pacman -Si'
+
+# query package information from the local repository
+alias pacQ='pacman -Qi'
+
+
+#
+# Search
+#
+
+# search for package in the remote repository
+alias pacs='pacman -Ss'
+
+# search for the package in the local repository
+alias pacS='pacman -Qs'
+
+
+#
+# Orphans
+#
+
+# list orphan packages
+alias pacol='pacman -Qdt'
+
+# remove orphan packages
+alias pacor='sudo pacman -Rns $(pacman -Qtdq)'
+
+
+#
+# Ownership
+#
+
+# list all files that belong to a package
+alias pacown='pacman -Ql'
+
+# show package(s) owning the specified file
+alias pacblame='pacman -Qo'
+
+#
+# Helpers
+#
+
 # source helper functions/aliases
 for helper ( ${zpacman_helper[@]} ); do
   if [[ -s ${0:h}/helper_${helper}.zsh ]]; then
@@ -35,5 +132,3 @@ for helper ( ${zpacman_helper[@]} ); do
   fi
 done
 
-# source pacman aliases
-source ${0:h}/alias.zsh
--- modules/syntax-highlighting/init.zsh
+++ modules/syntax-highlighting/init.zsh
@@ -2,7 +2,7 @@
 # enables fish-shell like syntax highlighting
 #
 
-source "${0:h}/external/zsh-syntax-highlighting.zsh" || return 1
-
 # highlighters
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(${zhighlighters[@]})
+
+source "${0:h}/external/zsh-syntax-highlighting.zsh" || return 1
