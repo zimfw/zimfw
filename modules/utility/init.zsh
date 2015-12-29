be99dc0971de763994265b3511a298e8f25c1335
Matt Hamilton <m@tthamilton.com>
2015-12-29 05:34:13 -0500

    add comments and fix BSD ls typo

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -8,6 +8,7 @@
 #
 
 if (( ${+commands[dircolors]} )); then
+  # GNU
   if [[ -s ${HOME}/.dir_colors ]]; then
     eval "$(dircolors --sh ${HOME}/.dir_colors)"
   else
@@ -17,11 +18,13 @@ if (( ${+commands[dircolors]} )); then
   alias ls='ls --group-directories-first --color=auto'
   
 else
-  # if we can't call dircolors, use generic preset
+  # BSD
+
+  # colors for ls and completion
   export LSCOLORS='exfxcxdxbxGxDxabagacad'
   export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 
-  alias ls='ls-G'
+  alias ls='ls -G'
 fi
 
 
