d13614e3bb5e24e832b4172e18902114f271f393
Matt Hamilton <m@tthamilton.com>
2016-01-08 09:16:22 -0500

    [prompt] change unset variable test
    The old test, [[ ${var} ]] tests for unset AND zero-length strings.
    
    Checking for zero length strings probably isn't that important. It is
    enough to ensure that the variable is set.
    
    Closes #18

--- modules/prompt/init.zsh
+++ modules/prompt/init.zsh
@@ -2,7 +2,7 @@
 # load user-defined prompt
 #
 
-if [[ ! ${TERM} == (linux|*bsd*|dumb) ]] && [[ ${zprompt_theme} ]]; then
+if [[ ! ${TERM} == (linux|*bsd*|dumb) ]] && (( ${+zprompt_theme} )); then
   autoload -Uz promptinit && promptinit
   prompt ${zprompt_theme}
 fi
