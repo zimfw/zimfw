d510f4f8619c59229a0fc1fcbf8ac27c4746c067
Georgy Yakovlev <git@sysdump.net>
2016-04-24 22:49:05 -0700

    Fix for OpenBSD ls alias https://github.com/Eriner/zim/issues/47
    Closes #48

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -31,7 +31,14 @@ else
   export LSCOLORS='exfxcxdxbxGxDxabagacad'
   export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 
-  alias ls='ls -G'
+  # stock OpenBSD ls does not support colors at all, but colorls does.
+  if [[ $OSTYPE == openbsd* ]]; then
+    if (( ${+commands[colorls]} )); then
+      alias ls='colorls -G'
+    fi
+  else
+    alias ls='ls -G'
+  fi
 fi
 
 
