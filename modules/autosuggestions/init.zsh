a7e060a4856543fd9addd930512a1f11948f2883
ℳichiel ℛoos <michiel@michielroos.com>
2017-05-03 16:05:37 +0200

    Add autosuggestions module

---
+++ modules/autosuggestions/init.zsh
@@ -0,0 +1,9 @@
+#
+# Fish-like fast/unobtrusive autosuggestions for zsh.
+#
+# It suggests commands as you type, based on command history.
+#
+
+# source script
+source ${0:h}/external/zsh-autosuggestions.zsh || return 1
+
