ccb265f7fef6fdcb5713ea7801f2ab08a2d7ecb4
Eric Nielsen <eric@amalgamar.com.br>
2017-07-30 18:24:58 -0500

    Don't load zim if zsh < 5.2
    and show an error message, as discussed in #184.
    
    Code in modules/environment/init.zsh is also simplified to only support
    zsh >= 5.2

--- init.zsh
+++ init.zsh
@@ -4,7 +4,8 @@
 
 autoload -Uz is-at-least
 if ! is-at-least 5.2; then
-  print "WARNING: Support for zsh < 5.2 will be removed in a future update. Update your version of zsh now to prevent any interruptions." >&2
+  print "ERROR: Zim didn't start. You're using zsh version ${ZSH_VERSION}, and versions < 5.2 are not supported. Update your zsh." >&2
+  return 1
 fi
 
 # Define zim location
