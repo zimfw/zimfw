730b69052af02aef21d2a37cccd10649416fde81
AtomicCoding <atomicgaming4life@gmail.com>
2018-04-05 10:58:59 -0700

    Updated ZCompiling

--- login_init.zsh
+++ login_init.zsh
@@ -23,32 +23,23 @@
     zrecompile -pq ${dir}.zwc ${dir}/^([_.]*|prompt_*_setup|README*|*.zwc|*.zwc.old)(-.N)
   done
 
-  # zcompile enabled module init scripts
+  # zcompile enabled module init scripts and subdirs
+  setopt nullglob
   for zmodule (${zmodules}); do
     zmodule_dir=${ZIM_HOME}/modules/${zmodule}
     if [[ -d ${zmodule_dir} ]]; then
-      for file (${zmodule_dir}/init.zsh \
-          ${zmodule_dir}/{,zsh-}${zmodule}.{zsh,plugin.zsh,zsh-theme,sh}); do
+      for file (${zmodule_dir}/**/*.{zsh,sh} \
+            ${zmodule_dir}/*.{zsh,sh}); do
         if [[ -f ${file} ]]; then
           zrecompile -pq ${file}
-          break
         fi
       done
     fi
   done
-
+  unsetopt nullglob
   # zcompile all prompt setup scripts
   for file in ${ZIM_HOME}/modules/prompt/functions/prompt_*_setup; do
     zrecompile -pq ${file}
   done
-
-  # syntax-highlighting
-  for file in ${ZIM_HOME}/modules/syntax-highlighting/external/highlighters/**^test-data/*.zsh; do
-    zrecompile -pq ${file}
-  done
-  zrecompile -pq ${ZIM_HOME}/modules/syntax-highlighting/external/zsh-syntax-highlighting.zsh
-
-  # zsh-histery-substring-search
-  zrecompile -pq ${ZIM_HOME}/modules/history-substring-search/external/zsh-history-substring-search.zsh
-
+  
 ) &!
