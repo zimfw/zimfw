6b6ec50cab2b0f26405e3376d40d845c6f4b911b
Eric Nielsen <eric@amalgamar.com.br>
2018-05-07 17:42:29 -0500

    [fasd] Dynamically create the cache file

--- modules/fasd/init.zsh
+++ modules/fasd/init.zsh
@@ -1,3 +1,9 @@
-source ${0:h}/external/fasd && source ${0:h}/cache.zsh || return 1
+source ${0:h}/external/fasd --init env || return 1
+
+local fasd_cache="${0:h}/cache.zsh"
+if [[ ! -e ${fasd_cache} ]]; then
+  fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install > ${fasd_cache}
+fi
+source ${fasd_cache} || return 1
 
 alias v='f -e vim -b viminfo'
