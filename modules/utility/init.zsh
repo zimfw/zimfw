17b41952ce217ee11a599c3136cb5d5111c55b98
Eric Nielsen <eric@amalgamar.com.br>
2018-05-24 20:39:49 -0500

    [utility] Revert "completion uses LS_COLORS for all OSTYPEs"
    This reverts commit 2f0243e5334dbeca66a0e9f1d8cb22558d76b9a4.
    
    Completion module does not depend on utility module anymore to set the
    zstyle list-colors. Making modules independent from each other as much
    as possible is a good thing.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -9,13 +9,14 @@
 if (( terminfo[colors] >= 8 )); then
 
   # ls Colours
-  # GNU colours are used by completion module for all OSTYPEs
-  (( ! ${+LS_COLORS} )) && export LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=30;41:sg=30;46:tw=30;42:ow=30;43'
-
   if (( ${+commands[dircolors]} )); then
     # GNU
 
-    [[ -s ${HOME}/.dir_colors ]] && eval "$(dircolors --sh ${HOME}/.dir_colors)"
+    (( ! ${+LS_COLORS} )) && if [[ -s ${HOME}/.dir_colors ]]; then
+      eval "$(dircolors --sh ${HOME}/.dir_colors)"
+    else
+      export LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+    fi
 
     alias ls='ls --group-directories-first --color=auto'
     alias lx='ll -X' # long format, sort by extension (GNU only)
