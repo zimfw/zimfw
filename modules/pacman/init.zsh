60b0910f6def220f1b115e3934088a6a21b538da
Bryan Hyshka <hyshka@users.noreply.github.com>
2018-10-19 07:32:34 -0600

    [pacman] Add pikaur to list of AUR helpers that handle SUID
    `pikaur` (https://github.com/actionless/pikaur) handles sudo
    authentication itself.
    
    Closes #302

--- modules/pacman/init.zsh
+++ modules/pacman/init.zsh
@@ -24,7 +24,7 @@ elif (( ! ${+commands[${zpacman_frontend}]} )); then
 You can fix this error by editing the 'zpacman_frontend' variable in your .zimrc" >&2
   zpacman_frontend='pacman'
   zpacman_frontend_priv='sudo pacman'
-elif [[ ${zpacman_frontend} == ("yaourt"|"pacaur"|"yay") ]]; then
+elif [[ ${zpacman_frontend} == ("yaourt"|"pacaur"|"yay"|"pikaur") ]]; then
   # those AUR helpers handle SUID themselves
   zpacman_frontend_priv="${zpacman_frontend}"
 else
